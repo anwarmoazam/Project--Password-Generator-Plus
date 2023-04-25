ecdf77e315c77ee7ae303a6aa54c48eb1a29f681
anwarmoazam <anwar.palimarwar@gmail.com>
2023-04-26 00:46:20 +0530

    project password generator

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let password = "";
 const numberSet = "1234567890";
 const lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
 const upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const symbolsSet = "";
+let symbolsSet = "";
 
 // selectors
 const length = document.getElementById('length');
@@ -14,6 +14,7 @@ const symbolInput = document.getElementById('symbols');
 const noSimilarCharacterInput = document.getElementById('nosimilarchar');
 const noDuplicateCharaterInput = document.getElementById('noduplicatechar');
 const noSequentialCharacterInput = document.getElementById('nosequentialchar');
+const result = document.getElementById('result');
 const generatePasswordBtn = document.getElementById('generate');
 const copyPasswordBtn = document.getElementById('copy');
 const symbols = document.getElementById('symbolstype');
@@ -28,6 +29,27 @@ const generatePassword = () => {
 }
 
 generatePasswordBtn.addEventListener('click',function(){
-    generatePassword();
+    let password = "";
+    while(password.length < length.value){
+        if(numberInput.checked && password.length < length.value) {
+            password += getRandomData(numberSet);
+            console.log("password in number",password);
+        }
+        if(lowerCaseInput.checked && password.length < length.value){
+            password += getRandomData(lowerCaseSet);
+            console.log("password in lowercase",password);
+        } 
+        if(upperCaseInput.checked && password.length < length.value){
+            password += getRandomData(upperCaseSet);
+            console.log("password in uppercase",password);
 
+        }
+        if(symbolInput.checked && password.length < length.value){
+            symbolsSet = symbols.value.trim();
+            console.log(symbolsSet);
+            password += getRandomData(symbolsSet);
+            console.log("password in symbol",password);
+        }
+    }
+    result.value = password;
 });
