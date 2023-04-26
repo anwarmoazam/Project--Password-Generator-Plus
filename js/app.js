a0f50e5114441dbe2faea3d9c746671527f1e13c
anwarmoazam <anwar.palimarwar@gmail.com>
2023-04-27 03:42:25 +0530

    similar character working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
-let password = "";
+/*
+I, l, 1, !
+0, O, o, D, Q
+5, S, s
+8, B
+6, b, G
+9, g, q
+2, Z, z
+m, n, r
+u, v, y
+p, d
+C, G, c, e
+a, e
+V, Y, W, w
+A, R, K, k
+x, X, *
+h, n, b
+*/
+
+// let password = "";
 const numberSet = "1234567890";
 const lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
 const upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let symbolsSet = "";
+let similarCharSet = [
+    ['I','l','1','!'],['0','O','o','D','Q'],['5','S','s'],['8','B'],['6','b','G'],['9','g','q'],
+    ['2','Z','z'],['m','n','r'],['u','v','y'],['p','d'],['C','G','c','e'],['a','e'],['V','Y','W','w'],
+    ['A','R','K','k'],['x','X','*'],['h','n','b']
+];
+let similarCharSet1 = ['Il1!','0OoDQ','5Ss','8B','6bG','9gq','2Zz','mnr','uvy','pd','CGce','ae','VYWw','ARKk','xX*','hnb'];
 
 // selectors
 const length = document.getElementById('length');
@@ -34,6 +59,27 @@ const generatePassword = (str) => {
         if(numberSet.includes(randomPassword[0]) || symbolsSet.includes(randomPassword[0])){
             onlyAlphabetSet = onlyAlphabetSet.concat(lowerCaseSet, upperCaseSet);
             randomPassword = randomPassword.replace(randomPassword[0],getRandomData(onlyAlphabetSet));
+        }
+    }
+    if(noSimilarCharacterInput.checked){
+        let char = "";
+        let temp = [];
+        for(let i=0; i<similarCharSet.length; i++){
+            for(let j=0; j<similarCharSet[i].length; j++){
+                if(randomPassword.includes(similarCharSet[i][j])){
+                    char = similarCharSet[i][j];
+                    temp = similarCharSet[i];
+                    let index = temp.indexOf(char);
+                    temp.splice(index,1);
+                    for(let k=0; k<temp.length; k++){
+                        if(randomPassword.includes(temp[k])){
+                            str = str.replace(temp[k],"");
+                            let newChar = getRandomData(str);
+                            randomPassword = randomPassword.replace(temp[k],newChar);
+                        }
+                    }
+                }
+            }
         }
     }
     return randomPassword;
@@ -68,6 +114,5 @@ const validateChecks = () => {
         symbolsSet = symbols.value.trim();
         str = str.concat(symbolsSet);
     }
-    
     return str;
 }
